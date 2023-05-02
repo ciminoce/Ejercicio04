14d99b63025cbddd364e1e54da4d3107b30c1465
Carlos Cimino <ciminoce@yahoo.com.ar>
2023-05-02 18:52:46 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio04.Consola/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+double lado;
+double superficie, perimetro;
+Console.Write("Ingrese el valor del lado del cuadrado:");
+lado= double.Parse(Console.ReadLine());
+superficie = Math.Pow(lado, 2);//eleva al cuadrado el valor del lado
+perimetro = lado * 4;
+Console.WriteLine($"La superficie del cuadrado de lado {lado} es {superficie}");
+//Console.WriteLine("La superficie del cuadrado es {0}",superficie); //forma anterior
+Console.WriteLine($"El perímetro del cuadrado de lado {lado} es {perimetro}");
